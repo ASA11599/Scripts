8cff4dc69008c00f0a53fea1c9c3998c8b50022a
Amayas Said Amer <saidameramayas@gmail.com>
2019-08-29 22:13:01 -0400

    add types

--- src/java-run.py
+++ src/java-run.py
@@ -8,19 +8,19 @@ if __name__ == "__main__":
     It deletes all class files after it is done.
     """
 
-    cmd_args = sys.argv[1:]
+    cmd_args: list = sys.argv[1:]
 
     if len(cmd_args) != 1:
         print("only one java file")
         exit()
     else:
-        jfile = cmd_args[0]
-        dirlst = os.listdir()
+        jfile: str = cmd_args[0]
+        dirlst: list = os.listdir()
         if not jfile in dirlst:
             print(jfile + " is not in " + os.getcwd())
             exit()
         else:
-            javac_exit_status = os.system("javac " + jfile)
+            javac_exit_status: int = os.system("javac " + jfile)
             if javac_exit_status == 2:
                 # this should never happen
                 print("the file does not exist")
@@ -28,7 +28,7 @@ if __name__ == "__main__":
                 print("compilation error")
             elif javac_exit_status == 0:
                 # make sure the class file was generated
-                java_program_name = jfile.split(".")[0]
+                java_program_name: str = jfile.split(".")[0]
                 if java_program_name + ".class" in os.listdir():
                     os.system("java " + java_program_name)
                     os.system("del *.class")
