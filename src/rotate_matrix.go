0f36319407e078d35270ea293d10f01582586289
ASA11599 <43392205+ASA11599@users.noreply.github.com>
2023-01-20 18:29:35 -0500

    Create rotate_matrix.go

---
+++ src/rotate_matrix.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+)
+
+func rotateMatrixOnce(matrix [][]int, direction int) [][]int {
+	if direction == 0 {
+		return matrix
+	} else if (direction == -1) || (direction == 1) {
+		nColumns := len(matrix[0])
+		nRows := len(matrix)
+		rotated := make([][]int, nColumns)
+		for i := range rotated {
+			rotated[i] = make([]int, nRows)
+		}
+		for row := range rotated {
+			for col := range rotated[row] {
+				if direction == 1 {
+					rotated[row][col] = matrix[nRows-1-col][row]
+				} else if direction == -1 {
+					rotated[row][col] = matrix[col][nColumns-1-row]
+				}
+			}
+		}
+		return rotated
+	} else {
+		return matrix
+	}
+}
+
+func rotateMatrix(matrix [][]int, rotation int) [][]int {
+	rotated := matrix
+	if rotation == 0 {
+		return rotated
+	} else {
+		if rotation < 0 {
+			for r := 0; r > rotation; r-- {
+				rotated = rotateMatrixOnce(rotated, -1)
+			}
+		} else if rotation > 0 {
+			for r := 0; r < rotation; r++ {
+				rotated = rotateMatrixOnce(rotated, 1)
+			}
+		}
+		return rotated
+	}
+}
+
+func printMatrix(matrix [][]int) {
+	for _, row := range matrix {
+		fmt.Println(row)
+	}
+}
+
+func main() {
+	matrix := [][]int{[]int{1, 2, 3}, []int{4, 5, 6}, []int{7, 8, 9}}
+	fmt.Println("matrix:")
+	printMatrix(matrix)
+	fmt.Println()
+	rotations := []int{0, 1, -1, 2, -2, 3, -3, 4, -4}
+	for i := range rotations {
+		fmt.Println("rotated clockwise", rotations[i], "times:")
+		printMatrix(rotateMatrix(matrix, rotations[i]))
+		fmt.Println()
+	}
+}
